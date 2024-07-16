35e86ae83aaa459407402861ecb1643be2f2c9b7
Mikhail <99434729+Zharik0v@users.noreply.github.com>
2024-07-16 23:40:12 +0300

    Update hw_3_3.py

--- HW_3/hw_3_3.py
+++ HW_3/hw_3_3.py
@@ -1,12 +1,16 @@
 try:
-    with open('input.txt') as file:
-        course = input('Course name: ')
-        student = []
-        for line in file.readlines():
-            all_students = line.split(':')
-            all_courses = all_students[1].strip('\n, ').split(', ')
-            if course in all_courses:
-                student.append(all_students[0].strip(' '))
-        print(student)
+    with open('cities.txt') as file_1:
+        with open('filtered_cities.txt', 'w') as file_2:
+            dic = {}
+            num = input('Please enter the minimum num of citizens: ')
+            while num.isdigit() != True:
+                num = input('Please enter the minimum num of citizens: ')
+            for line in file_1.readlines():
+                names = line.split(':')
+                dic[names[0]] = names[1].rstrip()
+            sorted_dic = dict(sorted(dic.items()))
+            for city in sorted_dic:
+                if int(sorted_dic[city]) > int(num):
+                    file_2.write(city + ':' + sorted_dic[city] + '\n')
 except:
-    print('File isn\'t found or there was an input error.')
+    print('File doesn\'t exist or there is another problem')
